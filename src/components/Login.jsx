73f48116e0969892023e1b8eb909bb83e25ebc1c
Arif Dikkanat <arifdikkanat2000@gmail.com>
2023-09-29 00:52:49 +0300

    Spotify Api Web Controls

---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import axios from 'axios';
+import {Buffer} from 'buffer';
+
+const client_id = 'ecb36e831a374db5992e7b3142da3178';
+const client_secret = 'd70452a8699447ee95e6a45da3e7e905';
+export default function Login() {
+
+  const handleClick = async () => {
+    const client_id = "ecb36e831a374db5992e7b3142da3178";
+    const redirect_uri = "http://localhost:3000/";
+    const api_uri = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-private",
+      "user-read-email",
+      "user-modify-playback-state",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "user-read-recently-played",
+      "user-top-read",
+    ];
+    window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+      " "
+    )}&response_type=token&show_dialog=true`;
+  };
+
+  return (
+    <Container>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
+        alt="spotify"
+      />
+      <button onClick={handleClick}>Connect Spotify</button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    background-color: black;
+    color: #49f585;
+    border: none;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;
